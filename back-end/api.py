ee5795a24332d3a05c59de94890b521c68e629c3
Alex Bloom <lehanbloom@gmail.com>
2020-06-09 19:05:32 +0300

    Changed to mock data so we can connect front and back end.

--- back-end/api.py
+++ back-end/api.py
@@ -1,8 +1,8 @@
 from flask_cors import CORS
 import json
 from flask import Flask, request
-from popular_times_functions import get_places_matching_category
-import copy
+from mock_data import mock_data
+
 
 app = Flask(__name__)
 CORS(app)
@@ -11,16 +11,8 @@ CORS(app)
 def place_recommendation():
     content = request.args
     category = content.get('category')
-    lat1 = float(content.get('latitude'))
-    long1 = float(content.get('longitude'))
-    lat2 = 34.772358
-    long2 = 32.061921
-    places = get_places_matching_category(category, lat1, long1, lat2, long2)
-    print (places)
-    response = app.response_class(response=json.dumps(places), status=200, mimetype="application/json")
+    response = app.response_class(response=json.dumps(mock_data), status=200, mimetype="application/json")
     return response
-
-
 
 
 if __name__ == "__main__":
--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -6,82 +6,88 @@ import { getlatlng } from "../lib/api";
 import { ApiKey } from "./api_key";
 
 class Map extends Component {
-	static defaultProps = {
-		center: {
-			// this are Tel Aviv cocrds
-			lat: 32.083333,
-			lng: 34.7999968,
-		},
-		zoom: 13,
-	};
-	state = {
-		inputPlace: "",
-		//itc coords
-		myPlaceCoordsLat: 32.053038,
-		myPlaceCoordsLng: 34.772103,
-	};
+  static defaultProps = {
+    center: {
+      // this are Tel Aviv cocrds
+      lat: 32.083333,
+      lng: 34.7999968,
+    },
+    zoom: 13,
+  };
+  state = {
+    inputPlace: "",
+    //itc coords
+    myPlaceCoordsLat: 32.052725,
+    myPlaceCoordsLng: 34.772358,
+  };
 
-	renderMarkers(map, maps) {
-		let marker = new maps.Marker({
-			position: {
-				lat: this.state.myPlaceCoordsLat,
-				lng: this.state.myPlaceCoordsLng,
-			},
-			map: map,
-			// title: "Hello World!",
-		});
-	}
+  renderMarkers(map, maps) {
+    let marker = new maps.Marker({
+      position: {
+        lat: this.state.myPlaceCoordsLat,
+        lng: this.state.myPlaceCoordsLng,
+      },
+      map: map,
+      // title: "Hello World!",
+    });
+  }
 
-	handleOnChange(event) {
-		console.log(event);
-		this.setState({ inputPlace: event });
-	}
+  handleOnChange(event) {
+    console.log(event);
+    this.setState({ inputPlace: event });
+  }
 
-	async handleOnSubmit() {
-		let { inputPlace, myPlaceCoordsLat, myPlaceCoordsLng } = this.state;
-		console.log("in the func");
-		// console.log(this.state.inputPlace); // this is the input from the user
-    let response = await getlatlng(inputPlace, myPlaceCoordsLat, myPlaceCoordsLng);
+  async handleOnSubmit() {
+    let { inputPlace, myPlaceCoordsLat, myPlaceCoordsLng } = this.state;
+    console.log("in the func");
+    // console.log(this.state.inputPlace); // this is the input from the user
+    let response = await getlatlng(
+      inputPlace,
+      myPlaceCoordsLat,
+      myPlaceCoordsLng
+    );
     console.log(response);
-	}
+  }
 
-	render() {
-		return (
-			<div className="row">
-			<div className="col-10 offset-1">
-				<InputGroup className="marginInput">
-					<FormControl
-						placeholder="Where Do You Want To Go?"
-						aria-label="Where Do You Want To Go?"
-						aria-describedby="basic-addon2"
-						onChange={(event) => this.handleOnChange(event.target.value)}
-					/>
-					<InputGroup.Append>
-						<Button
-							variant="primary"
-							onClick={() => {
-								this.handleOnSubmit();
-							}}
-						>
-							Button
-						</Button>
-					</InputGroup.Append>
-				</InputGroup>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-10 offset-1">
+          <InputGroup className="marginInput">
+            <FormControl
+              placeholder="Where Do You Want To Go?"
+              aria-label="Where Do You Want To Go?"
+              aria-describedby="basic-addon2"
+              onChange={(event) => this.handleOnChange(event.target.value)}
+            />
+            <InputGroup.Append>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  this.handleOnSubmit();
+                }}
+              >
+                Button
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
 
-				<div className="map">
-					<GoogleMapReact
-						bootstrapURLKeys={{
-							key: ApiKey,
-						}}
-						defaultCenter={this.props.center}
-						defaultZoom={this.props.zoom}
-						onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
-					></GoogleMapReact>
-				</div>
-			</div>
-			</div>
-		);
-	}
+          <div className="map">
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: ApiKey,
+              }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+              onGoogleApiLoaded={({ map, maps }) =>
+                this.renderMarkers(map, maps)
+              }
+            ></GoogleMapReact>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Map;
