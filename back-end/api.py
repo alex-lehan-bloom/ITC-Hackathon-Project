ab871ff36d2d46c8931898f457060d91b3b0fd3b
Alex Bloom <lehanbloom@gmail.com>
2020-06-09 16:31:40 +0300

    Minor changes to handle searching for places in Israel.

--- back-end/api.py
+++ back-end/api.py
@@ -13,16 +13,8 @@ def place_recommendation():
     category = content.get('category')
     lat1 = float(content.get('latitude'))
     long1 = float(content.get('longitude'))
-    lat2 = float(content.get('latitude2'))
-    long2 = float(content.get('longitude2'))
-    # lat1 = 36.150126
-    # long1 = -86.787082
-    # lat2 = 36.173166
-    # long2 = -86.779909
-    # category="bar"
-    # places = populartimes.get("AIzaSyBl1L1DdtZVL2tRjBRA6CcKw1Tp7pABjKQ", [category], (lat1, long1), (lat2, long2))
-    # places = populartimes.get("AIzaSyBl1L1DdtZVL2tRjBRA6CcKw1Tp7pABjKQ", [category], (lat1, long1), (lat2, long2))
-    # print(places)
+    lat2 = 34.772358
+    long2 = 32.061921
     places = get_places_matching_category(category, lat1, long1, lat2, long2)
     response = app.response_class(response=json.dumps(places), status=200, mimetype="application/json")
     return response
@@ -32,3 +24,5 @@ def place_recommendation():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
--- back-end/popular_times_functions.py
+++ back-end/popular_times_functions.py
@@ -3,8 +3,8 @@ import copy
 from api_key import api_key
 
 
-def get_places_matching_category(category, lat, long, lat2, long2):
-    places = populartimes.get(api_key, [category], (lat,long), (lat2,long2))
+def get_places_matching_category(category, lat1, long1, lat2, long2):
+    places = populartimes.get(api_key, ['bar'], (lat1, long1), (lat2, long2))
     return places
 
 
