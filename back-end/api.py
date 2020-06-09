66771269ea156b5238b0469729417908e927927d
Alex Bloom <lehanbloom@gmail.com>
2020-06-09 21:05:06 +0300

    Minor changes to flask route.

--- back-end/api.py
+++ back-end/api.py
@@ -14,7 +14,6 @@ def place_recommendation():
     response = app.response_class(response=json.dumps(mock_data), status=200, mimetype="application/json")
     return response
 
-
 if __name__ == "__main__":
     app.run(debug=True)
 
