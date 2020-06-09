0b2dbbc37c26ad3f3bc0af6b5ec76bfb9a6f9118
Alex Bloom <lehanbloom@gmail.com>
2020-06-09 15:10:00 +0300

    removing API key from back-end.

--- back-end/popular_times_functions.py
+++ back-end/popular_times_functions.py
@@ -4,7 +4,7 @@ from api_key import api_key
 
 
 def get_places_matching_category(category, lat, long, lat2, long2):
-    places = populartimes.get("AIzaSyBl1L1DdtZVL2tRjBRA6CcKw1Tp7pABjKQ", [category], (lat,long), (lat2,long2))
+    places = populartimes.get(api_key, [category], (lat,long), (lat2,long2))
     return places
 
 
