83164aff358b8ef1bf86d1386a7289741c8bc598
Alex Bloom <lehanbloom@gmail.com>
2020-06-10 13:03:42 +0300

    More test data.

--- back-end/api.py
+++ back-end/api.py
@@ -2,7 +2,8 @@ from flask_cors import CORS
 import json
 from flask import Flask, request
 from DatSci.Hackaton_poptimes_solver import min_pop
-from mock_data import mock_data
+from mock_data import mock_data, test_response
+from DatSci.current_best_fit import current_best_fit
 import datetime
 import datetime as dt
 
@@ -12,19 +13,26 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/place")
+def test():
+    response = app.response_class(response=json.dumps(test_response), status=200, mimetype="application/json")
+    return response
+
+
+
+@app.route("/place_test")
 def place_recommendation():
     content = request.args
     category = content.get('category').lower()
     day_of_week = datetime.datetime.today().weekday()
     current_hour = dt.datetime.now().hour
-    print(day_of_week)
-    print(current_hour)
+    if day_of_week == 2:
+        day_of_week == 'Wednesday'
     sublist = []
     for place in mock_data:
         if place['types'] == ["supermarket"]:
             sublist.append(place)
-    print(sublist)
-    recommendations = min_pop(sublist)
+    # recommendations = min_pop(sublist)
+    recommendations = current_best_fit(sublist, day_of_week, current_hour)
     # print(len(recommendations))
     response = app.response_class(response=json.dumps(recommendations), status=200, mimetype="application/json")
     return response
