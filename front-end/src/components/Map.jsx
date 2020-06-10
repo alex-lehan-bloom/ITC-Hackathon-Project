d9fa8e32733a61fe0d65ff92e2018bf915ba1a2b
Alex Bloom <lehanbloom@gmail.com>
2020-06-10 12:18:06 +0300

    Added name to markers so when you hover over marker it will display the name.

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -9,144 +9,144 @@ import MyLoader from "./Loader";
 import Places from "./Places";
 
 class Map extends Component {
-	static defaultProps = {
-		center: {
-			// this are Tel Aviv cocrds
-			lat: 32.083333,
-			lng: 34.7999968,
-		},
-		zoom: 13,
-	};
-	state = {
-		inputPlace: "",
-		//itc coords
-		myPlaceCoordsLat: 32.052725,
-		myPlaceCoordsLng: 34.772358,
-		loader: false,
-		data: [],
-		ArrayOfCoordinates: [1, 2, 3],
-		displayArrayOfCoordinates: false,
-		myLatLng: { lat: -25.363, lng: 131.044 },
-		test: [1, 2, 3],
-	};
+  static defaultProps = {
+    center: {
+      // this are Tel Aviv cocrds
+      lat: 32.083333,
+      lng: 34.7999968,
+    },
+    zoom: 13,
+  };
+  state = {
+    inputPlace: "",
+    //itc coords
+    myPlaceCoordsLat: 32.052725,
+    myPlaceCoordsLng: 34.772358,
+    loader: false,
+    data: [],
+    ArrayOfCoordinates: [1, 2, 3],
+    displayArrayOfCoordinates: false,
+    myLatLng: { lat: -25.363, lng: 131.044 },
+    test: [1, 2, 3],
+  };
 
-	renderMarkers(map, maps) {
-		let marker = new maps.Marker({
-			position: {
-				lat: this.state.myPlaceCoordsLat,
-				lng: this.state.myPlaceCoordsLng,
-			},
-			map: map,
-			// title: "Hello World!",
-		});
-		console.log("this.state.ArrayOfCoordinates");
-		console.log(this.state.ArrayOfCoordinates);
-	}
-	handleOnChange(event) {
-		console.log(event);
-		this.setState({ inputPlace: event });
-	}
+  renderMarkers(map, maps) {
+    let marker = new maps.Marker({
+      position: {
+        lat: this.state.myPlaceCoordsLat,
+        lng: this.state.myPlaceCoordsLng,
+      },
+      map: map,
+      // title: "Hello World!",
+    });
+    console.log("this.state.ArrayOfCoordinates");
+    console.log(this.state.ArrayOfCoordinates);
+  }
+  handleOnChange(event) {
+    console.log(event);
+    this.setState({ inputPlace: event });
+  }
 
-	async handleOnSubmit() {
-		let { inputPlace, myPlaceCoordsLat, myPlaceCoordsLng } = this.state;
-		console.log("in the func");
-		// console.log(this.state.inputPlace); // this is the input from the user
-		let response = await getlatlng(
-			inputPlace,
-			myPlaceCoordsLat,
-			myPlaceCoordsLng
-		);
-		this.setState({ data: response.data });
-		console.log("response.data");
-		console.log(response.data);
-	}
+  async handleOnSubmit() {
+    let { inputPlace, myPlaceCoordsLat, myPlaceCoordsLng } = this.state;
+    console.log("in the func");
+    // console.log(this.state.inputPlace); // this is the input from the user
+    let response = await getlatlng(
+      inputPlace,
+      myPlaceCoordsLat,
+      myPlaceCoordsLng
+    );
+    this.setState({ data: response.data });
+    console.log("response.data");
+    console.log(response.data);
+  }
 
-	handleCordinates(coordinates) {
-		console.log(coordinates);
-		this.setState(
-			{
-				ArrayOfCoordinates: coordinates,
-			},
-			() => {
-				console.log("test");
-				this.setState({ displayArrayOfCoordinates: true });
-			}
-		);
-	}
+  handleCordinates(coordinates) {
+    console.log(coordinates);
+    this.setState(
+      {
+        ArrayOfCoordinates: coordinates,
+      },
+      () => {
+        console.log("test");
+        this.setState({ displayArrayOfCoordinates: true });
+      }
+    );
+  }
 
-	render() {
-		let { data, test } = this.state;
-		return (
-			<>
-				<img
-					src="./images/coavoid-logo.png"
-					className="coavoidLogo"
-					alt="co(a)void-19"
-				/>
-				<div className="main-box flex">
-					<div className="row mapSize">
-						<div className="col-10 offset-1">
-							<InputGroup className="marginInput">
-								<FormControl
-									placeholder="Where Do You Want To Go?"
-									aria-label="Where Do You Want To Go?"
-									aria-describedby="basic-addon2"
-									onChange={(event) => this.handleOnChange(event.target.value)}
-								/>
-								<InputGroup.Append>
-									<Button
-										className="btnColor"
-										onClick={() => {
-											this.handleOnSubmit();
-										}}
-									>
-										Button
-									</Button>
-								</InputGroup.Append>
-							</InputGroup>
+  render() {
+    let { data, test } = this.state;
+    return (
+      <>
+        <img
+          src="./images/coavoid-logo.png"
+          className="coavoidLogo"
+          alt="co(a)void-19"
+        />
+        <div className="main-box flex">
+          <div className="row mapSize">
+            <div className="col-10 offset-1">
+              <InputGroup className="marginInput">
+                <FormControl
+                  placeholder="Where Do You Want To Go?"
+                  aria-label="Where Do You Want To Go?"
+                  aria-describedby="basic-addon2"
+                  onChange={(event) => this.handleOnChange(event.target.value)}
+                />
+                <InputGroup.Append>
+                  <Button
+                    className="btnColor"
+                    onClick={() => {
+                      this.handleOnSubmit();
+                    }}
+                  >
+                    Button
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
 
-							<div className="map">
-								<GoogleMapReact
-									bootstrapURLKeys={{
-										key: ApiKey,
-									}}
-									defaultCenter={this.props.center}
-									defaultZoom={this.props.zoom}
-									onGoogleApiLoaded={({ map, maps }) =>
-										this.renderMarkers(map, maps)
-									}
-								>
-									{this.state.displayArrayOfCoordinates &&
-										this.state.ArrayOfCoordinates.map((coordinates) => {
-											{
-												console.log(coordinates);
-											}
-											return (
-												<Marker
-													lat={coordinates.lat}
-													lng={coordinates.long}
-													name={coordinates.long}
-													color="red"
-												/>
-											);
-										})}
-									{/*  */}
-								</GoogleMapReact>
-								{this.state.loader === true && <MyLoader />}
-							</div>
-						</div>{" "}
-					</div>
+              <div className="map">
+                <GoogleMapReact
+                  bootstrapURLKeys={{
+                    key: ApiKey,
+                  }}
+                  defaultCenter={this.props.center}
+                  defaultZoom={this.props.zoom}
+                  onGoogleApiLoaded={({ map, maps }) =>
+                    this.renderMarkers(map, maps)
+                  }
+                >
+                  {this.state.displayArrayOfCoordinates &&
+                    this.state.ArrayOfCoordinates.map((coordinates) => {
+                      {
+                        console.log(coordinates);
+                      }
+                      return (
+                        <Marker
+                          lat={coordinates.lat}
+                          lng={coordinates.long}
+                          name={coordinates.name}
+                          color="red"
+                        />
+                      );
+                    })}
+                  {/*  */}
+                </GoogleMapReact>
+                {this.state.loader === true && <MyLoader />}
+              </div>
+            </div>{" "}
+          </div>
 
-					<Places
-						data={this.state.data}
-						handleCordinates={(cordinates) => {
-							this.handleCordinates(cordinates);
-						}}
-					/>
-				</div>
-			</>
-		);
-	}
+          <Places
+            data={this.state.data}
+            handleCordinates={(cordinates) => {
+              this.handleCordinates(cordinates);
+            }}
+          />
+        </div>
+      </>
+    );
+  }
 }
 
 export default Map;
--- front-end/src/components/Places.jsx
+++ front-end/src/components/Places.jsx
@@ -3,75 +3,79 @@ import Map from "./Map";
 import GaugeChart from "react-gauge-chart";
 
 function Places(props) {
-	const [data, setData] = useState();
-	const [coordinates, setCoordinates] = useState({});
+  const [data, setData] = useState();
+  const [coordinates, setCoordinates] = useState({});
 
-	useEffect(() => {
-		setData(props.data);
-	}, [props.data]);
+  useEffect(() => {
+    setData(props.data);
+  }, [props.data]);
 
-	useEffect(() => {
-		console.log("DATA");
-		let coords = [];
-		if (data != undefined) {
-			data.map((item) => {
-				console.log(item.coordinates.lat);
-				coords.push({ lat: item.coordinates.lat, long: item.coordinates.lng });
-			});
-		}
-		setCoordinates(coords);
-	}, [data]);
+  useEffect(() => {
+    console.log("DATA");
+    let coords = [];
+    if (data != undefined) {
+      data.map((item) => {
+        console.log(item.coordinates.lat);
+        coords.push({
+          lat: item.coordinates.lat,
+          long: item.coordinates.lng,
+          name: item.name,
+        });
+      });
+    }
+    setCoordinates(coords);
+  }, [data]);
 
-	useEffect(() => {
-		props.handleCordinates(coordinates);
-	}, [coordinates]);
+  useEffect(() => {
+    props.handleCordinates(coordinates);
+  }, [coordinates]);
 
-	return (
-		<>
-			{data !== undefined && (
-				<ul className="data-ul">
-					{data.map((item) => (
-						<>
-							<li key={item.id} className="place-in-list">
-								<div className="detail flexColum">
-									<div>
-										<b>{item.name}</b>
-									</div>
-								</div>
-								{/* <div className="chart flex">
+  return (
+    <>
+      {data !== undefined && (
+        <ul className="data-ul">
+          {data.map((item) => (
+            <>
+              <li key={item.id} className="place-in-list">
+                <div className="detail flexColum">
+                  <div>
+                    <b>{item.name}</b>
+                  </div>
+                </div>
+                {/* <div className="chart flex">
 									
                                 
                                     
 								</div> */}
-								<div className="rating flexColum">
-									<div>Population</div>
-									<div className="flex chart">
-										<GaugeChart
-											id={item.id}
-											className="gaugeChart"
-											style={{ width: "80%" }}
-											textColor={"rgb(0, 0, 0)"}
-											nrOfLevels={10}
-											colors={["#FF5F6D", "rgb(105, 189, 79)"]}
-											arcWidth={0.3}
-											percent={0.37}
-										/>
-									</div>
-								</div>
+                <div className="rating flexColum">
+                  <div>Population</div>
+                  <div className="flex chart">
+                    <GaugeChart
+                      id={item.id}
+                      className="gaugeChart"
+                      style={{ width: "80%" }}
+                      textColor={"rgb(0, 0, 0)"}
+                      nrOfLevels={10}
+                      colors={["#FF5F6D", "rgb(105, 189, 79)"]}
+                      arcWidth={0.3}
+                      percent={0.37}
+                    />
+                  </div>
+                </div>
 
-								<div className="rating flexColum">
-									<div>Rating</div>
-									<div className="ratingBox">{item.rating}</div>
-								</div>
-								{/* <div className="tweet-text">{item.coordinates.lat}</div>
+                <div className="rating flexColum">
+                  <div>Rating</div>
+                  <div className="ratingBox">{item.rating}</div>
+                </div>
+                {/* <div className="tweet-text">{item.coordinates.lat}</div>
 								<div className="tweet-text">{item.coordinates.lng}</div> */}
-							</li>
-						</>
-					))}
-				</ul>
-			)}
-		</>
-	);
+              </li>
+            </>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default Places;
