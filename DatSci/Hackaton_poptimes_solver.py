6c57e7b7c837eff92ce2d962332538a3c39821b9
Shachar M <s7mauda@gmail.com>
2020-06-10 13:07:09 +0300

    Update Hackaton_poptimes_solver.py

--- DatSci/Hackaton_poptimes_solver.py
+++ DatSci/Hackaton_poptimes_solver.py
@@ -30,9 +30,11 @@ def min_pop(data):
     best_id_day_time.rename(columns={0: 'hr'}, inplace = True)
     # add info
     inf_dat = pos_info.loc[pos_info['id'].isin(list(minimum_pop['id'])) ][infos_need]
+    inf_dat = pos_info.loc[pos_info['id'].isin(list(minimum_pop['id']))]
     respdata = pd.merge(best_id_day_time, inf_dat, left_on='id', right_on='id', how='outer' )
     respdata = respdata.to_dict('index')
     result_dict = [x for x in respdata.values()]
     return result_dict
 
 
+print(result_dict())
