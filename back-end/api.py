9080ed826c84330f90ec3560212e52e205a8decd
Alex Bloom <lehanbloom@gmail.com>
2020-06-10 11:19:45 +0300

    Created mock_data, filtered based on category, and sent to DS function.

--- back-end/api.py
+++ back-end/api.py
@@ -3,6 +3,9 @@ import json
 from flask import Flask, request
 from DatSci.Hackaton_poptimes_solver import min_pop
 from mock_data import mock_data
+import datetime
+import datetime as dt
+
 
 
 app = Flask(__name__)
@@ -11,10 +14,18 @@ CORS(app)
 @app.route("/place")
 def place_recommendation():
     content = request.args
-    category = content.get('category')
-    # print(mock_data)
-    # test = min_pop(mock_data)
-    response = app.response_class(response=json.dumps(mock_data), status=200, mimetype="application/json")
+    category = content.get('category').lower()
+    sublist = []
+    day_of_week = datetime.datetime.today().weekday()
+    current_hour = dt.datetime.now().hour
+    print(day_of_week)
+    print(current_hour)
+    for place in mock_data:
+        if place['types'] == ["supermarket"]:
+            sublist.append(place)
+    recommendations = min_pop(sublist)
+    # print(len(recommendations))
+    response = app.response_class(response=json.dumps(recommendations), status=200, mimetype="application/json")
     return response
 
 if __name__ == "__main__":
