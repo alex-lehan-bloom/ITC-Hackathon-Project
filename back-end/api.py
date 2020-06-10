984578e672935c05fe97e2f9abd7da783a5a02f6
Alex Bloom <lehanbloom@gmail.com>
2020-06-10 11:41:28 +0300

    Added sublist of mock_data for testing.

--- back-end/api.py
+++ back-end/api.py
@@ -15,14 +15,15 @@ CORS(app)
 def place_recommendation():
     content = request.args
     category = content.get('category').lower()
-    sublist = []
     day_of_week = datetime.datetime.today().weekday()
     current_hour = dt.datetime.now().hour
     print(day_of_week)
     print(current_hour)
+    sublist = []
     for place in mock_data:
         if place['types'] == ["supermarket"]:
             sublist.append(place)
+    print(sublist)
     recommendations = min_pop(sublist)
     # print(len(recommendations))
     response = app.response_class(response=json.dumps(recommendations), status=200, mimetype="application/json")
